39fb82459c5e72c31045dae9d514dc3d8eee4ed6
Supakorn Wrd <supakorn@proteus-tech.com>
2015-08-12 20:07:22 +0700

    Github webhook

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+//	"strconv"
 	"log"
 	"net/http"
 	"os"
+	"encoding/json"
 )
 
 type SlackPost struct {
@@ -56,6 +58,44 @@ func handlerThoth(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func handlerLeafbox(w http.ResponseWriter, r *http.Request) {
+	body,err := ioutil.ReadAll(r.Body)
+	if err!=nil {
+		log.Println(err)
+	}
+
+	var dat map[string]interface{}
+	if err := json.Unmarshal(body, &dat); err != nil {
+		panic(err)
+	}
+	//log.Println(dat)
+	//log.Println(dat["action"].(string))
+
+	// Case
+	xEvent := r.Header.Get("X-GitHub-Event")
+	switch xEvent {
+	case "pull_request":
+		log.Printf("PR #%d %s by @%s",int(dat["number"].(float64)),
+			dat["action"],
+			dat["sender"].(map[string]interface{})["login"])
+	case "pull_request_review_comment":
+		log.Printf("PR Comment #%d by @%s",int(dat["number"].(float64)),
+			dat["sender"].(map[string]interface{})["login"])
+	case "issues":
+		log.Printf("Issue #%d %s by @%s",int(dat["issue"].(map[string]interface{})["number"].(float64)),
+			dat["action"],
+			dat["sender"].(map[string]interface{})["login"])
+	case "issue_comment":
+		log.Printf("Issue Comment #%d by @%s",int(dat["issue"].(map[string]interface{})["number"].(float64)),
+			dat["sender"].(map[string]interface{})["login"])
+	default:
+		log.Println("case default")
+	}
+
+	fmt.Fprintf(w, "Leafbox body %s", dat)
+	//fmt.Fprintf(w, "Leafbox json %s", t)
+}
+
 func main() {
 	// Go Web
 	port := ":" + os.Getenv("GO_PORT")
@@ -71,5 +111,6 @@ func main() {
 
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/thoth", handlerThoth)
+	http.HandleFunc("/leafbox", handlerLeafbox)
 	http.ListenAndServe(port, nil)
 }
